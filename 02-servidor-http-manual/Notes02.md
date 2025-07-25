7c62d298ac74d41adb1c0f2415870f1b198cce54
DiegoMarchandon <diegomarc2020@gmail.com>
2025-07-24 23:33:49 -0300

    proyecto 2

--- 02-servidor-http-manual/Notes02.md
+++ 02-servidor-http-manual/Notes02.md
@@ -1,1 +1,14 @@
 # Servidor HTTP Básico sin Express
+modificando la URL del navegador siempre se tratará de una solicitud GET. si quiero enviar POST, puedo usar `fetch` desde la consola del navegador:
+```js
+fetch('http://localhost:3000/modulo/crear/MiTitulo', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ contenido: 'Texto de mi nota' })
+})
+.then(res => res.text())
+.then(data => console.log(data));
+```
+o también Postman, o un formulario con el método `POST`.
--- 02-servidor-http-manual/src/server.js
+++ 02-servidor-http-manual/src/server.js
@@ -5,16 +5,58 @@ import {listarNotas,crearNota,eliminarNota,leerNota} from './modulo.js';
 http.createServer(function(req,res){
     if(req.url === '/modulo/listar' && req.method === 'GET'){
         const notasListadas = listarNotas();
-        res.writeHead(200,{'Content-Type':'applicacion/json'});
+        // res.writeHead(200,{'Content-Type':'applicacion/json'});
         res.end(JSON.stringify(notasListadas));
-    }else if(req.url === '/modulo/leer' && req.method === 'GET'){
+    }else if(req.url.startsWith('/modulo/leer/') && req.method === 'GET'){
+        const params = new URLSearchParams(req.url.split('?')[1]);
+        const titulo = params.get('titulo');
+        const nota = leerNota(titulo);
+        // URL de consulta, ejemplo:
+        // /modulo/leer/?titulo=<titulo-de-la-nota></titulo-de-la-nota>
         
+        // const partes = req.url.split('/');
+        // const titulo = decodeURIComponent(partes[3]);
+        // const nota = leerNota(titulo);
+
+        if(nota){
+            res.writeHead(200,{'Content-Type':'application/json'});
+            res.end(JSON.stringify(nota));
+        }else{
+            res.writeHead(400,{'Content-Type':'text/plain'});
+            res.end('Nota no encontrada');
+        }
+    }else if(req.url.startsWith('/modulo/crear/') && req.method === 'POST'){
+        const partes = req.url.split('/');
+        const titulo = decodeURIComponent(partes[3]);
+        console.log("titulo: "+titulo);
+        const contenido = req.body.contenido;
+        console.log("contenido: "+contenido);	
+        const nota = crearNota(titulo,contenido);
+        if(nota){
+            res.writeHead(201,{'Content-Type':'application/json'});
+            res.end('Nota creada');
+        }else{
+            res.writeHead(400,{'Content-Type':'text/plain'});
+            res.end('Nota no encontrada');
+        }
+        return;
+    }else if(req.url.startsWith('/modulo/eliminar/') && req.method === 'DELETE'){
+        const partes = req.url.split('/');
+        const titulo = decodeURIComponent(partes[3]);
+        const nota = eliminarNota(titulo);
+        if(nota){
+            res.writeHead(201,{'Content-Type':'application/json'});
+            res.end('Nota creada');
+        }else{
+            res.writeHead(404,{'Content-Type':'text/plain'});
+            res.end('Nota no encontrada');
+        }
     }
 
-  res.writeHead(200,{'content-Type':'text/plain'});
+//   res.writeHead(200,{'content-Type':'text/plain'});
 //   mandamos una respuesta al cliente que hizo la request:
-  res.write("Hola mundo desde server.js");
-  res.end();  
+//   res.write("Hola mundo desde server.js");
+//   res.end();  
 }).listen(3000);
 
 console.log("servidor escuchando en http://localhost:3000");
